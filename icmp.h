54f24202a49dc269195a0949a088e91bf895921a
mish24 <poonammishra24398@gmail.com>
2018-02-27 14:05:21 +0000

    added icmp.h

--- icmp.h
+++ icmp.h
@@ -1,55 +1,13 @@
 /**
  *  icmp.h
  */
- 
-#ifndef icmp_guard
-#define icmp_guard
 
-// Maximum transmission unit
-#define MTU 1472
-
-struct icmp_packet
-{
-  char src_addr[100];
-  char dest_addr[100];
-  int type;
-  char *payload;
-  int payload_size;
-};
+#ifndef icmp_gaurd
+#define icmp_gaurd
 
 /**
- * Function to set packet type as ECHO
+ * Function to calculate checksum
  */
-void set_echo_type(struct icmp_packet *packet);
+uint16_t in_cksum(uint16_t *addr, int len);
 
-/**
- * Function to set packet type as REPLY
- */
-void set_reply_type(struct icmp_packet *packet);
-
-/**
- * Function to open a socket for icmp
- */
-int open_icmp_socket();
-
-/**
- * Function to bind the socket to INADDR_ANY
- */
-void bind_icmp_socket(int sock_fd);
-
-/**
- * Function to send ICMP Packet
- */
-void send_icmp_packet(int sock_fd, struct icmp_packet *packet_details);
-
-/**
- * Function to receive ICMP Packet
- */
-void receive_icmp_packet(int sock_fd, struct icmp_packet *packet_details);
-
-/**
- * Function to close the icmp socket
- */
-void close_icmp_socket(int sock_fd);
-
-#endif
+#endif 
