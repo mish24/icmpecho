ebfe0bec31603a464a342ea7a59d7e09ba6d844c
mish24 <poonammishra24398@gmail.com>
2018-02-28 23:04:54 +0000

    started writing the icmp library

--- icmp.c
+++ icmp.c
@@ -1,55 +1,115 @@
 /**
- *  icmp.h
+ *  icmp.c
  */
- 
-#ifndef icmp_guard
-#define icmp_guard
 
-// Maximum transmission unit
-#define MTU 1472
+#include <stdint.h>
+#include <string.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <unistd.h>
 
-struct icmp_packet
+// The socket fd
+int sockfd;
+
+/**
+ * Function to calculate checksum
+ */
+uint16_t in_cksum(uint16_t *addr, int len)
 {
-  char src_addr[100];
-  char dest_addr[100];
-  int type;
-  char *payload;
-  int payload_size;
-};
+  int nleft = len;
+  uint32_t sum = 0;
+  uint16_t *w = addr;
+  uint16_t answer = 0;
+
+  // Adding 16 bits sequentially in sum
+  while (nleft > 1) {
+    sum += *w;
+    nleft -= 2;
+    w++;
+  }
+
+  // If an odd byte is left
+  if (nleft == 1) {
+    *(unsigned char *) (&answer) = *(unsigned char *) w;
+    sum += answer;
+  }
+
+  sum = (sum >> 16) + (sum & 0xffff);
+  sum += (sum >> 16);
+  answer = ~sum;
+
+  return answer;
+}
 
 /**
- * Function to set packet type as ECHO
+ * Function to open the socket
  */
-void set_echo_type(struct icmp_packet *packet);
+void open_socket()
+{
+  int on = 1;
+
+  sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+
+  // Providing IP Headers
+  if (setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, (const char *)&on, sizeof(on)) == -1) {
+    printf("Unable to set IP_HDRINCL socket option\n");
+    exit(-1);
+  }
+}
 
 /**
- * Function to set packet type as REPLY
+ * Function to fill up common headers for IP and ICMP
  */
-void set_reply_type(struct icmp_packet *packet);
+void prepare_headers(struct iphdr *ip, struct icmphdr *icmp)
+{
+  memset(ip, 0, sizeof(ip);
+  memset(icmp, 0, sizeof(icmp);
 
-/**
- * Function to open a socket for icmp
- */
-int open_icmp_socket();
+  ip->version = 4;
+  ip->ihl = 5;
+  ip->tos = 0;
+  ip->id = rand();
+  ip->frag_off = 0;
+  ip->ttl = 255;
+  ip->protocol = IPPROTO_ICMP;
 
-/**
- * Function to bind the socket to INADDR_ANY
- */
-void bind_icmp_socket(int sock_fd);
+  icmp->type = ICMP_ECHO;
+  icmp->code = 0;
+  icmp->un.echo.sequence = rand();
+  icmp->un.echo.id = rand();
+  icmp->checksum = 0;
+}
 
 /**
  * Function to send ICMP Packet
  */
-void send_icmp_packet(int sock_fd, struct icmp_packet *packet_details);
+void send_packet(struct icmp_packet *packet_details)
+{
+  // Source and destination IPs
+  uint32_t src_addr;
+  uint32_t dest_addr;
 
-/**
- * Function to receive ICMP Packet
- */
-void receive_icmp_packet(int sock_fd, struct icmp_packet *packet_details);
+  struct iphdr *ip;
+  struct icmphdr *icmp;
 
-/**
- * Function to close the icmp socket
- */
-void close_icmp_socket(int sock_fd);
+  int packet_size;
+  char *packet;
 
-#endif
+  src_addr = inet_addr(packet_details->src_addr);
+  dest_addr = inet_addr(packet_details->dest_addr);
+
+  packet_size = sizeof(struct iphdr) + sizeof(struct icmphdr) + packet_details->payload_size;
+  packet = (char *)malloc(packet_size);
+
+  if (packet == NULL) {
+    printf("No memory available\n");
+    close_socket();
+    exit(-1);
+  }
+
+  prepare_headers(ip, icmp);
+
+  ip->tot_len = htons(packet_size);
+
+  icmp
+}
